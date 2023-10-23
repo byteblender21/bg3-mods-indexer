d711d4dcc1800a461d405b0da155e30b88bde28c
ByteBlender <byteblender21@gmail.com>
2023-10-23 19:16:36 +0200

    start with parsing

--- src/main/kotlin/parsing.kt
+++ src/main/kotlin/parsing.kt
@@ -1,8 +1,25 @@
 import java.io.File
 import java.nio.file.Files
 
+data class Entry(
+    val name: String,
+    val data: HashMap<String, String>
+)
+
 fun parseDataFile(file: File) {
+    val entries = ArrayList<Entry>()
+
     Files.readAllLines(file.toPath()).forEach { line ->
+        if (line.startsWith("new entry")) {
+            entries.add(Entry(
+                name = line
+                    .replace("new entry\"", "")
+                    .replace("\"", ""),
+                data = HashMap()
+            ))
+        }
         println(line)
     }
+
+//    println(entries)
 }
